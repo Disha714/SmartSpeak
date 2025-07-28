11546c4a880ec10201cbff6eabb8fea48446f64b
Disha714 <nsaidisha@gmail.com>
2025-07-28 16:40:52 +0530

    Add OpenAI test script

---
+++ test_openai.py
@@ -0,0 +1,12 @@
+import openai
+import os
+
+openai.api_key = os.getenv("OPENAI_API_KEY")  # use environment variable instead
+
+
+response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[{"role": "user", "content": "Hello, how are you?"}]
+)
+
+print(response['choices'][0]['message']['content'])
